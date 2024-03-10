b0285ac5f5ac35ae5afcef54cf5b42e9d57afd06
Tobi Akere <148399418+Tobidevs@users.noreply.github.com>
2024-03-10 12:09:40 -0500

    added saturn

--- home.js
+++ home.js
@@ -1,9 +1,11 @@
 let mainTitle = document.querySelector(".home h1");
 let stars = document.querySelector(".stars")
 let moon = document.querySelector(".moon")
+let saturn = document.querySelector(".saturn")
 
 window.addEventListener("scroll", () => {
     mainTitle.style.marginTop = window.scrollY * 1.3 + "px"
     stars.style.marginTop = window.scrollY * .8 + "px";
     moon.style.marginTop = window.scrollY * .7 + "px"
+    saturn.style.marginTop = window.scrollY * .5 + "px"
 })
