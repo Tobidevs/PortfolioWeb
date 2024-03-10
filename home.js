de01e0860f6867d1a57688ccd20fba137dbb181a
Tobi Akere <148399418+Tobidevs@users.noreply.github.com>
2024-03-10 04:55:13 -0500

    fixed upscaling2

--- home.js
+++ home.js
@@ -1,9 +1,9 @@
 let mainTitle = document.querySelector(".home h1");
 let stars = document.querySelector(".stars")
-let moon = document.querySelector("moon")
+let moon = document.querySelector(".moon")
 
 window.addEventListener("scroll", () => {
     mainTitle.style.marginTop = window.scrollY * 1.3 + "px"
     stars.style.marginTop = window.scrollY * .8 + "px";
-    moon.style.marginTop = window.scrollY * .5 + "px"
+    moon.style.marginTop = window.scrollY * .7 + "px"
 })
