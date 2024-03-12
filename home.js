fa1c9b4c3c90adcbd1be6ea1212d705cb600fa33
Tobi Akere <148399418+Tobidevs@users.noreply.github.com>
2024-03-11 23:12:07 -0500

    with scroll snap animation

--- home.js
+++ home.js
@@ -1,17 +1,20 @@
+// Home Page 
 let mainTitle = document.querySelector(".home h1");
 let stars = document.querySelector(".stars")
 let moon = document.querySelector(".moon")
 let saturn = document.querySelector(".saturn")
 let planet1 = document.querySelector(".planet1")
-let galaxyComet = document.querySelector(".galaxycomet")
+// let galaxyComet = document.querySelector(".galaxycomet")
 let alienblackhole = document.querySelector(".alienblackhole")
 
-window.addEventListener("scroll", () => {
+
+// Paralax animation for home page
+window.addEventListener("scroll", () => { 
     mainTitle.style.marginTop = window.scrollY * 1.3 + "px"
     stars.style.marginTop = window.scrollY * .8 + "px";
     moon.style.marginTop = window.scrollY * .7 + "px"
     saturn.style.marginTop = window.scrollY * .5 + "px"
     planet1.style.marginTop = window.scrollY * .3 + "px"
-    galaxyComet.style.marginTop = window.scrollY * .6 + "px"
-    alienblackhole.style.marginTop = window.scrollY * .4 + "px"
+    // galaxyComet.style.marginTop = window.scrollY * .6 + "px"
+    alienblackhole.style.marginTop = window.scrollY * .7 + "px"
 })
