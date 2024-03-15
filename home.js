7e4112689dbd760db86c42db3235eb5aa8baed6f
Tobi Akere <148399418+Tobidevs@users.noreply.github.com>
2024-03-14 22:52:23 -0500

    before revert committ

--- home.js
+++ home.js
@@ -3,12 +3,10 @@ let stars = document.querySelector(".stars")
 let moon = document.querySelector(".moon")
 let saturn = document.querySelector(".saturn")
 let planet1 = document.querySelector(".planet1")
-// let galaxyComet = document.querySelector(".galaxycomet")
+let galaxyComet = document.querySelector(".galaxycomet")
 let alienblackhole = document.querySelector(".alienblackhole")
 
-
-// Paralax animation for home page
-window.addEventListener("scroll", () => { 
+window.addEventListener("scroll", () => {
     mainTitle.style.marginTop = window.scrollY * 1.3 + "px"
     stars.style.marginTop = window.scrollY * .8 + "px";
     moon.style.marginTop = window.scrollY * .7 + "px"
@@ -16,4 +14,6 @@ window.addEventListener("scroll", () => {
     planet1.style.marginTop = window.scrollY * .3 + "px"
     // galaxyComet.style.marginTop = window.scrollY * .6 + "px"
     alienblackhole.style.marginTop = window.scrollY * .7 + "px"
+    galaxyComet.style.marginTop = window.scrollY * .6 + "px"
+    alienblackhole.style.marginTop = window.scrollY * .4 + "px"
 })
