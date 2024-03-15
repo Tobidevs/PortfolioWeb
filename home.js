b15821ed31c3fab1065437402f1de829c4fa6ef3
Tobi Akere <148399418+Tobidevs@users.noreply.github.com>
2024-03-14 23:00:04 -0500

    discard commit

--- home.js
+++ home.js
@@ -3,14 +3,13 @@ let stars = document.querySelector(".stars")
 let moon = document.querySelector(".moon")
 let saturn = document.querySelector(".saturn")
 let planet1 = document.querySelector(".planet1")
-let galaxyComet = document.querySelector(".galaxycomet")
-let alienblackhole = document.querySelector(".alienblackhole")
 
 window.addEventListener("scroll", () => {
     mainTitle.style.marginTop = window.scrollY * 1.3 + "px"
     stars.style.marginTop = window.scrollY * .8 + "px";
     moon.style.marginTop = window.scrollY * .7 + "px"
     saturn.style.marginTop = window.scrollY * .5 + "px"
+
     planet1.style.marginTop = window.scrollY * .3 + "px"
     // galaxyComet.style.marginTop = window.scrollY * .6 + "px"
     alienblackhole.style.marginTop = window.scrollY * .7 + "px"
@@ -20,4 +19,6 @@ window.addEventListener("scroll", () => {
     galaxyComet.style.marginTop = window.scrollY * .6 + "px"
     alienblackhole.style.marginTop = window.scrollY * .4 + "px"
 })
+
+
 
