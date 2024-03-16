b5bd05344d0b7927a1c9f4371f5c190236a4231f
Tobi Akere <148399418+Tobidevs@users.noreply.github.com>
2024-03-15 23:26:24 -0500

    half complete clouds

--- home.js
+++ home.js
@@ -23,9 +23,9 @@ const observer = new IntersectionObserver((entries) => {
         if (entry.isIntersecting) {
             entry.target.classList.add('show')
         }
-        else {
-        entry.target.classList.remove('show')
-        }
+        // else {
+        // entry.target.classList.remove('show')
+        // }
     })
 })
 hiddenElements.forEach((el) => observer.observe(el))
